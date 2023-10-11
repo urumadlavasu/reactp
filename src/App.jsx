d5c85f5c256282418e5a754aef6b1e1063cec601
urumadlavasu <urumadlavasu04@gmail.com>
2023-10-11 16:39:29 +0530

    sd

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import Counter from './features/counter/Counter';
-import Todolist from './features/counter/todolist/Todolist';
 
 function App() {
   return (
     <div className='mybox'>
-      <Todolist></Todolist>
+     
       <h1>app</h1>
-      <Counter></Counter>
+      <h2>reactapp</h2>
+      
     </div>
   );
 }
