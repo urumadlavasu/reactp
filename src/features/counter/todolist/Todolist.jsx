85275cedb0a2d656c13e3be71d16090e7de1695f
urumadlavasu <urumadlavasu04@gmail.com>
2023-10-11 18:23:31 +0530

    todolistsuccess

--- src/features/counter/todolist/Todolist.jsx
+++ src/features/counter/todolist/Todolist.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { addTodo, deleteTodo } from './todoSlice'
 
 function Todolist() {
    var {todolist} = useSelector(state=>state.t)
    console.log(todolist)
+  var dispatch = useDispatch()
+  var [newtodo,setNewtodo] = useState('')
   return (
     <div className='mybox'>
         <h1>Todolist</h1>
-        <input type="text" onChange={()=>{}} />
+        <input type="text" onChange={(e)=>{setNewtodo(e.target.value)}} />
+        <button onClick={()=>{dispatch(addTodo(newtodo))}} >Addtodo</button>
+        
+        {
+            todolist?.map((todo,i)=>{
+              return <li>
+                          {todo}
+                          <button onClick={()=>{dispatch(deleteTodo(i))}}>Deletetodo</button>
+                    </li>
+            })
+        }
     </div>
   )
 }
